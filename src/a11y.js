dd42d8e24ce8674c6457f2e3755b917758923b88
Alex Page <alex@alexpage.com.au>
2017-12-05 19:41:05 +1100

    Fixing npm issues

--- src/a11y.js
+++ src/a11y.js
@@ -27,7 +27,7 @@ import { CheckColor, GetHsl, GetHex, GetContrast }  from './color';
  *
  * @return {string}            - The closest hexadecimal color for `toMakeA11y` on `background` to meet contrast requirements
  */
-export default function A11yColor( toMakeA11y, background, ratioKey = 'small', steps = 0.1 ) {
+const A11yColor = ( toMakeA11y, background, ratioKey = 'small', steps = 0.1 ) => {
 	Log.verbose( `Finding closest A11yColor() for ${ toMakeA11y } on ${ background }` );
 
 	const ratios = {
@@ -88,3 +88,6 @@ export default function A11yColor( toMakeA11y, background, ratioKey = 'small', s
 
 	throw new Error( 'Color cannot be found with current settings' );
 }
+
+
+module.exports = A11yColor;
